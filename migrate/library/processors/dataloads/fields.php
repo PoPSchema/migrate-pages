4fb2de37b2bb98bef8e16f697a5f5d015306a16c
Leo <losoviz@gmail.com>
2019-09-13 10:48:21 +0800

    Typed functions

--- migrate/library/processors/dataloads/fields.php
+++ migrate/library/processors/dataloads/fields.php
@@ -4,14 +4,14 @@ class PoP_Pages_Module_Processor_FieldDataloads extends \PoP\ComponentModel\PoP_
 {
     public const MODULE_DATALOAD_DATAQUERY_PAGE_FIELDS = 'dataload-dataquery-page-fields';
 
-    public function getModulesToProcess()
+    public function getModulesToProcess(): array
     {
         return array(
             [self::class, self::MODULE_DATALOAD_DATAQUERY_PAGE_FIELDS],
         );
     }
 
-    public function getDataloaderClass(array $module)
+    public function getDataloaderClass(array $module): ?string
     {
         switch ($module[1]) {
             case self::MODULE_DATALOAD_DATAQUERY_PAGE_FIELDS:
