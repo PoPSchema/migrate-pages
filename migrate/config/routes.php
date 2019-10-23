07f59379e92b7fc15ab0f3c3ecd8c535b9bd1843
Leo <losoviz@gmail.com>
2019-10-23 13:46:51 +0800

    Migrated definitions

--- migrate/config/routes.php
+++ migrate/config/routes.php
@@ -1,13 +1,15 @@
 <?php
 use PoP\Hooks\Facades\HooksAPIFacade;
+use PoP\Definitions\Facades\DefinitionManagerFacade;
+$definitionManager = DefinitionManagerFacade::getInstance();
 
 // Loader Pages
 //--------------------------------------------------------
 if (!defined('POP_PAGES_ROUTE_LOADERS_PAGES_FIELDS')) {
-	define('POP_PAGES_ROUTE_LOADERS_PAGES_FIELDS', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('loaders/pages/fields', POP_DEFINITIONGROUP_ROUTES));
+	define('POP_PAGES_ROUTE_LOADERS_PAGES_FIELDS', $definitionManager->getUniqueDefinition('loaders/pages/fields', POP_DEFINITIONGROUP_ROUTES));
 }
 if (!defined('POP_PAGES_ROUTE_LOADERS_PAGES_LAYOUTS')) {
-	define('POP_PAGES_ROUTE_LOADERS_PAGES_LAYOUTS', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('loaders/pages/layouts', POP_DEFINITIONGROUP_ROUTES));
+	define('POP_PAGES_ROUTE_LOADERS_PAGES_LAYOUTS', $definitionManager->getUniqueDefinition('loaders/pages/layouts', POP_DEFINITIONGROUP_ROUTES));
 }
 
 HooksAPIFacade::getInstance()->addFilter(
