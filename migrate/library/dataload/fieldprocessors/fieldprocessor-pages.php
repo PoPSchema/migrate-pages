5ae076abb1e727ad0de63d24fc7b4692874005ae
Leo <losoviz@gmail.com>
2019-12-08 17:48:50 +0800

    Renamed DB_KEY to TYPE_COLLECTION_NAME

--- migrate/library/dataload/fieldprocessors/fieldprocessor-pages.php
+++ migrate/library/dataload/fieldprocessors/fieldprocessor-pages.php
@@ -5,11 +5,11 @@ use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 
 class TypeResolver_Pages extends AbstractTypeResolver
 {
-    public const DATABASE_KEY = 'pages';
+    public const TYPE_COLLECTION_NAME = 'pages';
 
-    public function getDatabaseKey(): string
+    public function getTypeCollectionName(): string
     {
-        return self::DATABASE_KEY;
+        return self::TYPE_COLLECTION_NAME;
     }
 
     public function getId($resultItem)
